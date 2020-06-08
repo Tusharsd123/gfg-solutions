cdd5351e5702005f2efa5fe372e038730fdeb8ae
Tusharsd123 <44468541+Tusharsd123@users.noreply.github.com>
2020-06-08 12:29:55 +0530

    Create stock-buy-sell-1.cpp

---
+++ basic-array/stock-buy-sell-1.cpp
@@ -0,0 +1,54 @@
+int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int profit=0;
+        //whenever u buy the stock sell it as soon as possible(greedy approach).
+        for(int i = 1;i<n;i++)
+        {
+            if(prices[i]>prices[i-1])
+            {
+                profit += prices[i] - prices[i-1];
+             }
+        }
+        
+        return profit;
+    }
+};
+
+//to calculate the indices
+void stockBuySell(int price[], int n) 
+{ 
+    // Prices must be given for at least two days 
+    if (n == 1) 
+        return; 
+  
+    // Traverse through given price array 
+    int i = 0; 
+    while (i < n - 1) { 
+  
+        // Find Local Minima 
+        // Note that the limit is (n-2) as we are 
+        // comparing present element to the next element 
+        while ((i < n - 1) && (price[i + 1] <= price[i])) 
+            i++; 
+  
+        // If we reached the end, break 
+        // as no further solution possible 
+        if (i == n - 1) 
+            break; 
+  
+        // Store the index of minima 
+        int buy = i++; 
+  
+        // Find Local Maxima 
+        // Note that the limit is (n-1) as we are 
+        // comparing to previous element 
+        while ((i < n) && (price[i] >= price[i - 1])) 
+            i++; 
+  
+        // Store the index of maxima 
+        int sell = i - 1; 
+  
+        cout << "Buy on day: " << buy 
+             << "\t Sell on day: " << sell << endl; 
+    } 
+} 
