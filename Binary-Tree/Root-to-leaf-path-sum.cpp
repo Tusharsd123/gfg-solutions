49ce4a96503be937d624cc3c6f35aa3c1c40aac4
Tusharsd123 <44468541+Tusharsd123@users.noreply.github.com>
2020-06-09 18:00:06 +0530

    Update Root-to-leaf-path-sum.cpp

--- Binary-Tree/Root-to-leaf-path-sum.cpp
+++ Binary-Tree/Root-to-leaf-path-sum.cpp
@@ -1,3 +1,10 @@
+//if(node == NULL || sum ==0)//this line will only execute if we reach at any end of some leaf node without sum being zero 
+//OR sum became 0 at non leaf node..
+//if(!node->right && !node ->left) return (sum - node->data == 0)
+//it checks whether this is leaf node and if yes then check whether sum here is 0...if 0 then path is found whose cumulative sum == sum.
+//return (hasPathSum(node->left,sum - node->data) || hasPathSum(node->right ,sum - node->data)
+//this is further recursion going down on both ends until we either we get to leaf or sum becomes zero or to the end of branch...
+
 bool hasPathSum(Node *root, int sum)
 {
    if(root==NULL || sum==0)
@@ -9,4 +16,4 @@ bool hasPathSum(Node *root, int sum)
    
 }
 
-Time-complexity is O(n)
+//Time-complexity is O(n)
