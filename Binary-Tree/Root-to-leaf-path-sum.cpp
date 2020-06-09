9bc4b750f3bd0d4b00e4129911a29cee0bfe72d1
Tusharsd123 <44468541+Tusharsd123@users.noreply.github.com>
2020-06-09 17:56:59 +0530

    Create Root-to-leaf-path-sum.cpp

---
+++ Binary-Tree/Root-to-leaf-path-sum.cpp
@@ -0,0 +1,12 @@
+bool hasPathSum(Node *root, int sum)
+{
+   if(root==NULL || sum==0)
+            return 0;
+   if(!root->left && !root->right) 
+         return (sum-root->data == 0);
+   
+   return( hasPathSum(root->left,sum-root->data)|| hasPathSum(root->right,sum-root->data) );
+   
+}
+
+Time-complexity is O(n)
