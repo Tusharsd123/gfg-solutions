952d34228efa9cfed9acf413c37134d94d024d99
Tusharsd123 <44468541+Tusharsd123@users.noreply.github.com>
2020-06-09 19:59:22 +0530

    Update longest-common-prefix.cpp

--- basic-array/longest-common-prefix.cpp
+++ basic-array/longest-common-prefix.cpp
@@ -1,3 +1,4 @@
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -32,3 +33,42 @@ int main()
 		   cout<<a.substr(0,i)<<endl;
 	}
 }
+
+
+
+//divide and conquer approach to same problem
+
+string commonPrefixUtil(string str1, string str2) 
+{ 
+    string result; 
+    int n1 = str1.length(), n2 = str2.length(); 
+  
+    for (int i=0, j=0; i<=n1-1&&j<=n2-1; i++,j++) 
+    { 
+        if (str1[i] != str2[j]) 
+            break; 
+        result.push_back(str1[i]); 
+    } 
+    return (result); 
+} 
+  
+// A Divide and Conquer based function to find the 
+// longest common prefix. This is similar to the 
+// merge sort technique 
+string commonPrefix(string arr[], int low, int high) 
+{ 
+    if (low == high) 
+        return (arr[low]); 
+  
+    if (high > low) 
+    { 
+        // Same as (low + high)/2, but avoids overflow for 
+        // large low and high 
+        int mid = low + (high - low) / 2; 
+  
+        string str1 = commonPrefix(arr, low, mid); 
+        string str2 = commonPrefix(arr, mid+1, high); 
+  
+        return (commonPrefixUtil(str1, str2)); 
+    } 
+} 
