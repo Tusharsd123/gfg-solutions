e63af5fa6d8b4aa3bb7d925d960502455cfefc03
Tusharsd123 <44468541+Tusharsd123@users.noreply.github.com>
2020-06-10 18:32:16 +0530

    Create Gas-Station.cpp

---
+++ Greedy/Gas-Station.cpp
@@ -0,0 +1,20 @@
+int tour(petrolPump p[],int n)
+{
+   int start = 0;
+   int end= 1;
+   int curr_petrol = p[start].petrol-p[start].distance;
+   while(start!=end || curr_petrol<0)
+   {
+       while(curr_petrol<0 && start!=end)
+       {
+           curr_petrol -= p[start].petrol-p[start].distance;
+           start = (start+1)%n;
+           if(start==0)
+           return -1;
+       }
+       
+           curr_petrol += p[end].petrol-p[end].distance;
+           end = (end+1)%n;
+   }
+   return start;
+}
