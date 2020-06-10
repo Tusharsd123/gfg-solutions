3bbf7fb3be1615aee8b2a83e56c77222d767ffcb
Tusharsd123 <44468541+Tusharsd123@users.noreply.github.com>
2020-06-10 20:32:02 +0530

    Create bubble-sort.cpp

---
+++ sorting-algos/bubble-sort.cpp
@@ -0,0 +1,32 @@
+Worst and Average Case Time Complexity: O(n^2). Worst case occurs when array is reverse sorted.
+
+Best Case Time Complexity: O(n). Best case occurs when array is already sorted.
+
+Auxiliary Space: O(1)
+
+Boundary Cases: Bubble sort takes minimum time (Order of n) when elements are already sorted.
+
+Sorting In Place: Yes
+
+
+void bubbleSort(int arr[], int n) 
+{ 
+   int i, j; 
+   bool swapped; 
+   for (i = 0; i < n-1; i++) 
+   { 
+     swapped = false; 
+     for (j = 0; j < n-i-1; j++) 
+     { 
+        if (arr[j] > arr[j+1]) 
+        { 
+           swap(&arr[j], &arr[j+1]); 
+           swapped = true; 
+        } 
+     } 
+  
+     // IF no two elements were swapped by inner loop, then break 
+     if (swapped == false) 
+        break; 
+   } 
+} 
