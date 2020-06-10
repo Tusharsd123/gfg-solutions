f3db9ed93e139f1157213261a1e9362c0e12e17e
Tusharsd123 <44468541+Tusharsd123@users.noreply.github.com>
2020-06-10 20:23:47 +0530

    Create selection-sort.cpp

---
+++ sorting-algos/selection-sort.cpp
@@ -0,0 +1,23 @@
+Time Complexity: O(n^2) as there are two nested loops.
+
+Auxiliary Space: O(1)
+The good thing about selection sort is it never makes more than O(n) swaps and can be useful when memory write is a costly operation.
+
+
+void selectionSort(int arr[], int n)  
+{  
+    int i, j, min_idx;  
+  
+    // One by one move boundary of unsorted subarray  
+    for (i = 0; i < n-1; i++)  
+    {  
+        // Find the minimum element in unsorted array  
+        min_idx = i;  
+        for (j = i+1; j < n; j++)  
+        if (arr[j] < arr[min_idx])  
+            min_idx = j;  
+  
+        // Swap the found minimum element with the first element  
+        swap(&arr[min_idx], &arr[i]);  
+    }  
+}  
