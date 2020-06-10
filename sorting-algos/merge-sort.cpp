caf863e3b39647fcddf8d0ad9e73a0b89c98a861
Tusharsd123 <44468541+Tusharsd123@users.noreply.github.com>
2020-06-10 20:44:03 +0530

    Create merge-sort.cpp

---
+++ sorting-algos/merge-sort.cpp
@@ -0,0 +1,60 @@
+/*Time Complexity: Sorting arrays on different machines. Merge Sort is a recursive algorithm and time complexity can be expressed as following recurrence relation.
+T(n) = 2T(n/2) + \Theta(n)
+The above recurrence can be solved either using Recurrence Tree method or Master method. 
+It falls in case II of Master Method and solution of the recurrence is \Theta(nLogn).
+Time complexity of Merge Sort is \Theta(nLogn) in all 3 cases (worst, average and best) 
+as merge sort always divides the array into two halves and take linear time to merge two halves.*/
+
+Auxiliary Space: O(n)
+
+Algorithmic Paradigm: Divide and Conquer
+
+void merge(int *Arr, int start, int mid, int end) {
+	// create a temp array
+	int temp[end - start + 1];
+
+	// crawlers for both intervals and for temp
+	int i = start, j = mid+1, k = 0;
+
+	// traverse both arrays and in each iteration add smaller of both elements in temp 
+	while(i <= mid && j <= end) {
+		if(Arr[i] <= Arr[j]) {
+			temp[k] = Arr[i];
+			k += 1; i += 1;
+		}
+		else {
+			temp[k] = Arr[j];
+			k += 1; j += 1;
+		}
+	}
+
+	// add elements left in the first interval 
+	while(i <= mid) {
+		temp[k] = Arr[i];
+		k += 1; i += 1;
+	}
+
+	// add elements left in the second interval 
+	while(j <= end) {
+		temp[k] = Arr[j];
+		k += 1; j += 1;
+	}
+
+	// copy temp to original interval
+	for(i = start; i <= end; i += 1) {
+		Arr[i] = temp[i - start]
+	}
+}
+
+// Arr is an array of integer type
+// start and end are the starting and ending index of current interval of Arr
+
+void mergeSort(int *Arr, int start, int end) {
+
+	if(start < end) {
+		int mid = (start + end) / 2;
+		mergeSort(Arr, start, mid);
+		mergeSort(Arr, mid+1, end);
+		merge(Arr, start, mid, end);
+	}
+}
